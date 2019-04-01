01ab7b9b928f8cccb9f4d9a9d140b37fa72bf38d
fsorodrigues <fsorodrigues@gmail.com>
2019-04-01 00:18:29 -0400

    First commit

---
+++ start-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function startenv() {
+    ENVPATH=$"/home/$USER/envs/"
+
+    if [ $1 == "--list" ]
+        then
+            ls -l $ENVPATH | grep "^d" | rev | cut -d " " -f 1 | rev
+        else
+            . $ENVPATH/$1/bin/activate
+    fi
+}
+
